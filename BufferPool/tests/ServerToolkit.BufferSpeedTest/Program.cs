7fd227ae7fedb3bc7f23f2078007b1731b5339a3
Sunny Ahuwanya <sahuwanya@gmail.com>
2012-09-26 22:49:46 -0400

    Added a BufferSpeedTest project which is useful for comparing performance of different versions.

---
+++ BufferPool/tests/ServerToolkit.BufferSpeedTest/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServerToolkit.BufferManagement;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ServerToolkit.BufferPoolSpeedTest
+{
+    class Program
+    {
+        static BufferPool pool = new BufferPool(1024 * 1024, 1, 1);
+
+        const int bufferCount = 20;
+        const int operationsPerThread = 1000000;
+        const int minBufferSize = 27000;
+        const int maxBufferSize = 30000;
+        const int NoOfThreads = 4;
+        const int loops = 10;
+
+        static ManualResetEvent startEvent;
+        static ManualResetEvent[] doneEvents;
+
+        static Random rand = new Random();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running test...");
+
+            TimeSpan totalTestTime = new TimeSpan();
+
+            for (int p = 0; p < loops; p++)
+            {
+                Stopwatch timer = new Stopwatch();
+
+                startEvent = new ManualResetEvent(false);
+                doneEvents = new ManualResetEvent[NoOfThreads];
+
+                for (int i = 0; i < NoOfThreads; i++)
+                {
+                    var done = doneEvents[i] = new ManualResetEvent(false);
+                    Thread t = new Thread(() =>
+                    {
+                        RunTest(done);
+                    });
+
+                    t.IsBackground = true;
+                    t.Start();
+
+                }
+
+                Thread.Sleep(500); //Wait for all threads to be ready
+
+                timer.Start(); //start timer
+
+                startEvent.Set(); //Signal start 
+
+                WaitHandle.WaitAll(doneEvents); //Wait for all done signals
+
+                timer.Stop(); //stop timer
+
+                totalTestTime += timer.Elapsed; //add elapsed time to total time
+
+            }
+
+            
+
+            Console.WriteLine("\r\nSpeed test complete. {0} loops over {1:N0} buffer operations across {2} threads took {3}\r\n", loops, operationsPerThread * NoOfThreads, NoOfThreads, totalTestTime);
+        }
+
+        static void RunTest(ManualResetEvent done)
+        {
+            //Wait for signal
+            startEvent.WaitOne();
+
+            IBuffer[] buffers = new IBuffer[bufferCount];
+
+            //Grab new buffers
+            for (int i = 0; i < buffers.Length; i++)
+            {
+                buffers[i] = pool.GetBuffer(minBufferSize + rand.Next(101) * ((maxBufferSize - minBufferSize) / 100));
+            }
+
+            //Randomly allocate and deallocate buffers
+            int selected;
+            for (int i = 0; i < operationsPerThread; i++)
+            {
+                selected = rand.Next(bufferCount);
+
+                if (buffers[selected].IsDisposed)
+                {
+                    buffers[selected] = pool.GetBuffer(minBufferSize + rand.Next(101) * ((maxBufferSize - minBufferSize) / 100));
+                }
+                else
+                {
+                    buffers[selected].Dispose();
+                }
+            }
+
+            //Dispose all buffers
+            for (int i = 0; i < buffers.Length; i++)
+            {
+                buffers[i].Dispose();
+            }
+
+            //Signal you're done
+            done.Set();
+
+        }
+    }
+}
---
+++ BufferPool/tests/ServerToolkit.BufferSpeedTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServerToolkit.BufferPoolSpeedTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServerToolkit.BufferPoolSpeedTest")]
+[assembly: AssemblyCopyright("Copyright © Sunny Ahuwanya 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bb0ee8b6-ed02-481f-b10e-f9b354a5a01c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
